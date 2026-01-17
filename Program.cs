ac37c8a3fe5ca4acc3b92092a224418c13300c24
ArtSkellige <ArtSkelligeBravoZulu141@protonmail.com>
2026-01-17 10:11:49 +0300

    Nested iteration and selection statements.

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿string[] ListOfOrderIDs = [ "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179"];
+foreach (string orderID in ListOfOrderIDs)
+{
+    if (orderID.StartsWith("B"))
+    {
+        Console.WriteLine($"The orderID {orderID} starts with a 'B' letter!");
+    }
+}
